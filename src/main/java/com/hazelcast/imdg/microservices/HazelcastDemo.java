5fd04b4376bbe2512b5f863c5f4f0e042e855030
Nicolas Frankel <nicolas@frankel.ch>
2019-08-28 09:44:55 +0200

    Showcase Hazelcast through JCache

--- src/main/java/com/hazelcast/imdg/microservices/HazelcastDemo.java
+++ src/main/java/com/hazelcast/imdg/microservices/HazelcastDemo.java
@@ -1,14 +1,20 @@
 package com.hazelcast.imdg.microservices;
 
+import com.hazelcast.config.CacheConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.ManagementCenterConfig;
 import com.hazelcast.config.MapConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableCaching
 public class HazelcastDemo {
 
     @Bean
@@ -22,6 +28,16 @@ public class HazelcastDemo {
         config.setManagementCenterConfig(managementCenterConfig);
         config.setProperty("jmx.enabled", "true");
         return config;
+    }
+
+    @Bean
+    public JCacheManagerCustomizer customizer() {
+        return cacheManager -> {
+            CacheConfig<Long, Person> config = new CacheConfig<>("service");
+            config.setKeyType(Long.class);
+            config.setValueType(Person.class);
+            cacheManager.createCache("service", config);
+        };
     }
 
     public static void main(String[] args) {
