ec76931b52e11ae6f83dd9e0db089a95f48abdd2
Nicolas Frankel <nicolas@frankel.ch>
2019-08-28 09:44:55 +0200

    Showcase Hazelcast through Spring's caching annotations

--- src/main/java/com/hazelcast/imdg/microservices/HazelcastDemo.java
+++ src/main/java/com/hazelcast/imdg/microservices/HazelcastDemo.java
@@ -1,6 +1,5 @@
 package com.hazelcast.imdg.microservices;
 
-import com.hazelcast.config.CacheConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.InMemoryFormat;
 import com.hazelcast.config.ManagementCenterConfig;
@@ -9,7 +8,7 @@ import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.spring.cache.HazelcastCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 
@@ -22,6 +21,9 @@ public class HazelcastDemo {
         Config config = new Config("hazelcastInstance");
         MapConfig databaseConfig = new MapConfig("database");
         config.addMapConfig(databaseConfig);
+        MapConfig serviceConfig = new MapConfig("service");
+        serviceConfig.setInMemoryFormat(InMemoryFormat.OBJECT);
+        config.addMapConfig(serviceConfig);
         ManagementCenterConfig managementCenterConfig = new ManagementCenterConfig();
         managementCenterConfig.setEnabled(true);
         managementCenterConfig.setUrl("http://localhost:8081");
@@ -31,13 +33,8 @@ public class HazelcastDemo {
     }
 
     @Bean
-    public JCacheManagerCustomizer customizer() {
-        return cacheManager -> {
-            CacheConfig<Long, Person> config = new CacheConfig<>("service");
-            config.setKeyType(Long.class);
-            config.setValueType(Person.class);
-            cacheManager.createCache("service", config);
-        };
+    public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+        return new HazelcastCacheManager(hazelcastInstance);
     }
 
     public static void main(String[] args) {
