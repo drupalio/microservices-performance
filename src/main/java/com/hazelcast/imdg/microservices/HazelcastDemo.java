0b3270e442a14f24f00f5c3b77b365ee00c107db
Nicolas Frankel <nicolas@frankel.ch>
2019-08-28 09:55:39 +0200

    Replace XML-based configuration with code-based configuration

--- src/main/java/com/hazelcast/imdg/microservices/HazelcastDemo.java
+++ src/main/java/com/hazelcast/imdg/microservices/HazelcastDemo.java
@@ -1,10 +1,28 @@
 package com.hazelcast.imdg.microservices;
 
+import com.hazelcast.config.Config;
+import com.hazelcast.config.InMemoryFormat;
+import com.hazelcast.config.ManagementCenterConfig;
+import com.hazelcast.config.MapConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HazelcastDemo {
+
+    @Bean
+    public Config config() {
+        Config config = new Config("hazelcastInstance");
+        MapConfig databaseConfig = new MapConfig("database");
+        config.addMapConfig(databaseConfig);
+        ManagementCenterConfig managementCenterConfig = new ManagementCenterConfig();
+        managementCenterConfig.setEnabled(true);
+        managementCenterConfig.setUrl("http://localhost:8081");
+        config.setManagementCenterConfig(managementCenterConfig);
+        config.setProperty("jmx.enabled", "true");
+        return config;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HazelcastDemo.class, args);
